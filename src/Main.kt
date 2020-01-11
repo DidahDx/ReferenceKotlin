bab6fca75ff3c52c57fe064499157aad74c7dd4b
DidahdDx <didahdaniel@gmail.com>
2020-01-11 22:12:01 +0300

    how to use when

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,18 @@ var age = 123   //type inference
 
 fun main() {
     println(name)
-    println(gate)
     println(age)
+
+    if (gate != null) {
+        println(gate)
+    }
+
+    // when similar to switch in java
+    when (gate) {
+        null -> println("Hey")
+        else -> println(gate)
+    }
+
+    val greeting = if (gate!=null) gate else "hey"
+    println(greeting)
 }
