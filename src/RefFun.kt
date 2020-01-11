743219e9a32c6d8aed5e3c6a5b2d72c58d6aa5e3
DidahdDx <didahdaniel@gmail.com>
2020-01-11 15:08:02 +0300

    Functions

---
+++ src/RefFun.kt
@@ -0,0 +1,51 @@
+fun main() {
+    println("test " + getGreeting() + " " + someGreeting())
+    sayHello()
+    hello("World")
+    hello("where is the", "World")
+    makingArrays()
+}
+
+fun getGreeting(): String {
+    return "Function call"
+}
+
+//Unit is used when fun does not return anything
+fun sayHello(): Unit {
+    println("Hello")
+}
+
+fun hello(customGreeting: String) {
+    println("Hello $customGreeting")
+}
+
+//fun using string template
+fun hello(greeting: String, customGreeting: String) = println("$greeting $customGreeting")
+
+//single expression of fun and type inference
+fun someGreeting() = "Hi"
+
+//arrays and list
+fun makingArrays() {
+    //replacing arrayOf with listOf works similar to array
+    val things = arrayOf("cars", "books", "table", "pen")
+    val thingsList = listOf("cars", "books", "table", "pen","list","container")
+    println(things.size)
+    println(thingsList.size)
+    println(things.iterator())
+
+    for (t in things) {
+        println(t)
+    }
+
+    things.forEach { println(it) }
+    thingsList.forEach { println(it) }
+
+    things.forEachIndexed { index, thing -> println("$thing is at index $index")  }
+
+
+    //maps
+    val map= mapOf(1 to "a", 2 to "e", 3 to "l")
+    map.forEach { (key, value) -> println("$key -> $value")  }
+
+}
