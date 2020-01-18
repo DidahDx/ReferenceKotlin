2ff926ef47a0b605c5f07f209c3b26843397be2b
Daniel Didah <didahdaniel@gmail.com>
2020-01-18 17:22:39 +0300

    interface and casting

--- src/PersonInfoProvider.kt
+++ src/PersonInfoProvider.kt
@@ -1,17 +1,59 @@
 interface PersonInfoProvider {
-    fun printInfo(person: Person)
-}
-
-//implementing an interface
-class BasicInfo : PersonInfoProvider {
-    override fun printInfo(person: Person) {
+    val provider: String
+    fun printInfo(person: Person) {
+        //basic default implementation of the interface
+        println(provider)
         person.PrintInfo()
     }
 }
 
+interface SessionInfoProvider{
+    fun getSessionId():String
+}
+
+
+//implementing an interface
+class BasicInfo : PersonInfoProvider, SessionInfoProvider {
+    override fun getSessionId(): String {
+        return "12"
+    }
+
+    override val provider: String
+        get() = "Basic"
+
+    //overiding with a default implementation
+    override fun printInfo(person: Person) {
+        super.printInfo(person)
+        println("New statement")
+    }
+
+//    override fun printInfo(person: Person) {
+//        person.PrintInfo()
+//    }
+}
+
 //using
 fun main() {
-    val provider=BasicInfo()
+    val provider = BasicInfo()
 
     provider.printInfo(Person())
+    println(provider.getSessionId())
+    checkTypes(provider)
+}
+
+//checking the types of
+fun checkTypes(infoProvider: PersonInfoProvider){
+    if (infoProvider is SessionInfoProvider){
+        println("$infoProvider is a session")
+
+        //casting using keyword as
+        (infoProvider as SessionInfoProvider).getSessionId()
+
+        //smart casting as long as type is SessionInfoProvider
+        infoProvider.getSessionId()
+
+
+    }else{
+        println("$infoProvider is not session")
+    }
 }
