5839b00a80be1096cb38ef6c4a216511dcf51947
Daniel Didah <didahdaniel@gmail.com>
2020-01-18 17:03:47 +0300

    interface implementation

--- src/PersonInfoProvider.kt
+++ src/PersonInfoProvider.kt
@@ -1,2 +1,17 @@
 interface PersonInfoProvider {
+    fun printInfo(person: Person)
+}
+
+//implementing an interface
+class BasicInfo : PersonInfoProvider {
+    override fun printInfo(person: Person) {
+        person.PrintInfo()
+    }
+}
+
+//using
+fun main() {
+    val provider=BasicInfo()
+
+    provider.printInfo(Person())
 }
