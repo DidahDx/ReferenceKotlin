9dd575c8b29bf848dbcd00281e316fd7bf6b3abf
Daniel Didah <didahdaniel@gmail.com>
2020-01-18 16:43:57 +0300

    classes visibilty

--- src/KotlinClasses.kt
+++ src/KotlinClasses.kt
@@ -3,6 +3,11 @@
 
 //        val person=Person("sdfg","dfg")
         val person=Person()
-        println(person.name)
-        println(person.sona)
+//        println(person.name)
+//        println(person.sona)
+//        person.NickName="dx"
+//        person.NickName="DxD"
+//        println(person.NickName)
+
+        person.PrintInfo()
     }
--- src/Person.kt
+++ src/Person.kt
@@ -1,5 +1,31 @@
 //defining properites of the class using val
 //and assigning default values
-class Person(val name:String="Name",val sona:String="sona"){
+class Person(val name: String = "Name", val sona: String = "sona") {
 
+    var NickName: String? = null
+        set(value) {
+            //defining a custom set
+            field = value
+
+            //defining new logic
+            println("the name $value")
+        }
+        get() {
+            println("returned the value $field")
+            return field
+        }
+
+    //method
+    fun PrintInfo() {
+        //  val nicName=if (NickName!=null) NickName else "No nickname"
+        //elvis operator
+        val nicName = NickName ?: "No nickname"
+        println("$name ($nicName) $sona")
+    }
 }
+
+//for val only getters are generated
+//for var setters and getters are generated
+//visibility in kotlin is public by default
+// other visibility (internal) (protected) (private)
+//protected can only be accessed within that class or in subclasses
