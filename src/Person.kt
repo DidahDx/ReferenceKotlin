86a2422cc6188e3bc1060396fe225a7c4acae30f
Daniel Didah <didahdaniel@gmail.com>
2020-01-18 15:45:00 +0300

    default values

--- src/Person.kt
+++ src/Person.kt
@@ -1,17 +1,5 @@
 //defining properites of the class using val
-class Person(val name:String,val sona:String){
-
-    init {
-        println("init 1")
-    }
-
-    //secondary constructor with default values
-    constructor():this("Name","sona"){
-        println("Secondary con")
-    }
-
-    init {
-        println("init 2")
-    }
+//and assigning default values
+class Person(val name:String="Name",val sona:String="sona"){
 
 }
